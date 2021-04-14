c26b37e4276cfad45ed6844779830fc16b680295
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-04-14 22:19:57 +0100

    add a go up button

--- src/Browser.svelte
+++ src/Browser.svelte
@@ -1,8 +1,9 @@
 <script lang="typescript">
 	import { onMount } from "svelte";
 	import { each } from "svelte/internal";
+	import { get } from "svelte/store";
 
-	import { mdiDotsHorizontal } from "@mdi/js";
+	import { mdiDotsHorizontal, mdiChevronDoubleUp } from "@mdi/js";
 	import * as icon from "./icon";
 
 	import getDirContent from "./getDirContent";
@@ -61,16 +62,35 @@
 		{/each}
 	</ul>
 {:else}
+	<!-- Add a go up button if we are not in the root -->
+	{#if $directory.length != 0}
+		<button
+			class="flex items-center p-4 bg-yellow-300 w-full"
+			on:click={() => {
+				$directory = get(directory).slice(0, get(directory).length - 1)
+				update()
+
+			}}
+		>
+			<svg
+				class="fill-current mr-2"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				width="24px"><path d={mdiChevronDoubleUp} /></svg
+			>
+			<p>Go up</p>
+		</button>
+	{/if}
 	<ul class="md:grid md:grid-cols-3 lg:grid-cols-5 md:gap-4 md:m-4">
 		{#each directoryContent as item, i}
 			<li>
 				<button
-					on:click={() => { 
-						if (item.name.endsWith('/')) {
-							directory.set([...$directory, item.name])
+					on:click={() => {
+						if (item.name.endsWith("/")) {
+							directory.set([...$directory, item.name]);
 							update();
 						}
-						}}
+					}}
 					class="border-b-2 flex md:inline items-center p-4 min-h-6  border-gray-600 md:border-0 md:text-center text-center justify-start md:rounded-lg w-full hover:bg-green-50 bg-gray-200 cursor-pointer transition-all hover:shadow-md"
 				>
 					<div class="md:flex md:justify-center md:items-center">
@@ -81,7 +101,6 @@
 							width="24px"><path d={item.iconPath} /></svg
 						>
 					</div>
-
 					<p>{item.name}</p>
 				</button>
 			</li>
