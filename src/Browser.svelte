1d8152fa4826bd114aa7e39742ebaa24f254f192
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-04-14 21:37:03 +0100

    reafactor each File to a component

--- src/Browser.svelte
+++ src/Browser.svelte
@@ -1,7 +1,7 @@
 <script lang="typescript">
 	import { onMount } from "svelte";
 	import { each } from "svelte/internal";
-
+	import File from "./File.svelte";
 	import { mdiDotsHorizontal } from "@mdi/js";
 	import * as icon from "./icon";
 
@@ -64,26 +64,7 @@
 	<ul class="md:grid md:grid-cols-3 lg:grid-cols-5 md:gap-4 md:m-4">
 		{#each directoryContent as item, i}
 			<li>
-				<button
-					on:click={() => { 
-						if (item.name.endsWith('/')) {
-							directory.set([...$directory, item.name])
-							update();
-						}
-						}}
-					class="border-b-2 flex md:inline items-center p-4 min-h-6  border-gray-600 md:border-0 md:text-center text-center justify-start md:rounded-lg w-full hover:bg-green-50 bg-gray-200 cursor-pointer transition-all hover:shadow-md"
-				>
-					<div class="md:flex md:justify-center md:items-center">
-						<svg
-							class="fill-current mr-2"
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							width="24px"><path d={item.iconPath} /></svg
-						>
-					</div>
-
-					<p>{item.name}</p>
-				</button>
+				<File {item} />
 			</li>
 		{/each}
 	</ul>
