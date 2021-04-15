5bc86ddb2c4349b1c6d877a2f23c9774ebdce53d
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-04-15 12:10:44 +0100

    make text white in dark mode

--- src/stores.ts
+++ src/stores.ts
@@ -2,12 +2,12 @@
 import { writable } from 'svelte/store';
 
 export const gridStyle = {
-  container: 'md:grid md:grid-cols-3 lg:grid-cols-5 md:gap-4 md:m-4',
+  container: 'dark:text-white md:grid md:grid-cols-3 lg:grid-cols-5 md:gap-4 md:m-4',
   entry: 'p-4 md:border-0 md:text-center md:rounded-lg border-b-2 h-full flex items-center dark:bg-gray-800 border-gray-600 text-center justify-start w-full hover:bg-green-50 bg-gray-200 transition-all hover:shadow-md',
 };
 
 export const listStyle = {
-  container: '',
+  container: 'dark:text-white',
   entry: 'justify-start p-4 border-b-2 h-full flex items-center dark:bg-gray-800 border-gray-600 text-center w-full hover:bg-green-50 bg-gray-200 transition-all hover:shadow-md',
 };
 
